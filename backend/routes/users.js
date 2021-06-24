fd581d71122a5b027d86b2f5fa1c35d9eb347512
Shantanu Kulkarni <shantanukulkarni100@gmail.com>
2021-06-24 12:41:18 +0530

    updated changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,52 +4,65 @@ const User = require('../Models/UserModel');
 const router = express.Router();
 
 //Login
-router.post('/login',async(req,res)=>{
-    const user ={
-    email : req.body.email,
-     password : req.body.password
+router.post('/login', async (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
     }
- if (user){
-     res.json(user)
- }
- else{
-     res.send("User not found");
- }
+ 
+    if (user) {
+        res.json(user)
+    }
+    else {
+        res.send("User not found");
+    }
 });
 
 //Register
-router.post('/register',(req,res)=>{
-   const users = new User({
-       firstName : req.body.firstName,
-       lastName : req.body.lastName,
-       email : req.body.email,
-       password: req.body.password
-   })
+router.post('/register', async (req, res) => {
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    })
 
-   try{
-       console.log("in try of register/post")
-       const user1 = users.save()
-       res.json(user1)
-   }catch(err){
-       res.send("error")
-   }
+
+    try {
+
+        const user1 = await User.findOne({ email: user.email });
+        if (user1) {
+
+            res.json({
+                message: "Username already exists",
+            });
+            return;
+        }
+        else {
+
+            await user.save()
+            res.send("user created")
+        }
+    } catch (err) {
+        res.send("error" + err)
+    }
 })
 
 //Delete
-router.delete('/:id', async(req,res)=>{
-   try{
-       const user =  await User.findById(req.params.id)
-       const user1 =  user.remove()
-       res.json(user1)
-   }catch(err){
-       res.send("error" + err);
-   }
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const user1 = user.remove()
+        res.json(user1)
+    } catch (err) {
+        res.send("error" + err);
+    }
 })
 
 
 //Home
-router.get('/Home',(req,res)=>{
-   res.send('This is Home');
+router.get('/Home', (req, res) => {
+    res.send('This is Home');
 })
 
 

06a000c8fb99263838c67465f0e7a78708010077
pratikparamane98 <56255793+pratikparamane98@users.noreply.github.com>
2021-08-20 16:41:07 +0530

    pratik ui commit login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React,{ useState , useContext } from 'react';
 import { useHistory } from 'react-router';
 import { CredentialsContext } from "../App";
+import LoginImg from '../../src/login.svg';
 //import './page.scss';
 import { Link,  } from "react-router-dom";
 import Header from '../header/Header';
@@ -15,17 +16,13 @@ export const handleErrors = async (response) => {
   };
 
 export default function Login(){
-
-    const [loading,setloading] = useState(false);
-
     const [email,setemail] = useState("");
     const [password,setPassword] = useState("");
     const [error, setError] = useState("");
     const [, setCredentials] = useContext(CredentialsContext);
-    
+    const  [loginStatus, setloginStatus] = useState(false);
 
     const login =(e) =>{
-        setloading(true);
         e.preventDefault();
         fetch(`http://localhost:4000/users/login`,
             {
@@ -40,6 +37,7 @@ export default function Login(){
         })
         .then(handleErrors)
         .then((response) => {
+            setloginStatus(true);
             localStorage.setItem("token",response.token)
             setCredentials({
               email,
@@ -49,52 +47,107 @@ export default function Login(){
         .catch((error) =>{
             setError(error.message);
         });
-
-        setTimeout(() => {
-            setloading(false);
-          }, 8000);
     };
         const history = useHistory();
-     
+       //Button
+    const ClickAuth = () => {
+        fetch(`http://localhost:4000/users/isUserAuth`,
+        {
+            method: "GET",
+            headers: {
+                "x-access-token": localStorage.getItem("token"),
+              "Content-Type": "application/json",
+        },
+       })
+       .then(handleErrors)
+       .then(() => {
+        history.push("/home");
+        })
+       .catch((error) =>{
+        setError(error.message);
+       });
+    }
 
-    return(
-        <div className="app">
-        <div><Header/></div>
+    // return(
+    //     <div className="app">
+    //     <div><Header/></div>
+       
+    //     {loginStatus && <button className="btn btn-success" onClick={ClickAuth}>Check if auth</button>}
 
-        <div className="row App-conatiner zoomEffect">
-            <div className="col-6 container-fluid" data-aos="zoom-in">
-               <form onSubmit={login}>
-                <h3 data-aos="zoom-in"><i class="fas fa-hands-helping"></i> Helpo LogIn</h3>
+    //     <div className="row App-conatiner">
+    //         <div className="col-6 container-fluid">
+    //            <form onSubmit={login}>
+    //             <h3><i class="fas fa-hands-helping"></i> Helpo LogIn</h3>
                
-                <p className=" mt-5">
-                    New User?? <Link  to="/register">Register</Link>
-                </p>
+    //             <p className=" mt-5">
+    //                 New User?? <Link  to="/register">Register</Link>
+    //             </p>
                 
-                <div className="form-group">
-                    <input required type="email" className="form-control input-line" placeholder="email" onChange={(e) => setemail(e.target.value)}/>
-                </div>
+    //             <div className="form-group">
+    //                 <input required type="email" className="form-control input-line" placeholder="email" onChange={(e) => setemail(e.target.value)}/>
+    //             </div>
 
-                <div className="form-group">
-                    <input required minLength="5" type="password" className="form-control input-line" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-
-                {loading && <button type="submit" className="btn btn-secondary btn-lg btn-block"><i class="fa fa-spinner fa-spin"></i> Sign in</button>}
-                {!loading && <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>}
-                
+    //             <div className="form-group">
+    //                 <input required minLength="5" type="password" className="form-control input-line" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+    //             </div>
 
                 
-                {error && <span id="reg-msg" >{error}</span>}
+    //             <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
 
-                <p className="forgot-password text-right">
-                    Forgot <Link  to="/reset">password</Link>
-                </p>
-            </form>
-            </div>
-            <div className="col">
-             
+                
+    //             {error && <span id="reg-msg" >{error}</span>}
+
+    //             <p className="forgot-password text-right">
+    //                 Forgot <Link  to="/reset">password</Link>
+    //             </p>
+    //         </form>
+    //         </div>
+            
+    //     </div>
+    //     </div>
+    // )
+    return(
+        <div className="background">
+            <div className="container">
+            <div className="base-container">
+                <form onSubmit={login}>
+                    <div className="content">
+                        <div className="header">Welcome back</div>
+
+                            <div className="image">
+                                <img src={LoginImg} />
+                            </div>
+                            <div className="form">
+                                <div className="form-group">
+                                <label htmlFor="username">Username</label>
+                                {/* <input type="text" name="username" placeholder="username" /> */}
+                                <input required type="email" className="form-control input-line" placeholder="username" onChange={(e) => setemail(e.target.value)}/>
+                                </div>
+                                <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                {/* <input type="password" name="password" placeholder="password" /> */}
+                                <input required minLength="5" type="password" className="form-control input-line" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+                                </div>
+                            </div>
+                            <div className="footer">
+                                <button type="submit" className="btn">
+                                    Login
+                                </button>
+                            </div>
+                            <div className="password">
+                                <p>forgot your password?<Link  to="/reset">Reset password</Link></p>
+                            </div>
+                            <div className="account">
+                                {/* <p>-------------------or-------------------</p> */}
+                                <p>or</p>
+                            </div>
+                            <div className="account">
+                                <p><Link  to="/register">Create New Account</Link></p>
+                            </div>
+                    </div> 
+                </form>
             </div>
         </div>
-       
         </div>
     )
 }

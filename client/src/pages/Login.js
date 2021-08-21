fa9462d1cf8a2a349c4afdedcb3b4fa2ccd2afd8
pratikparamane98 <56255793+pratikparamane98@users.noreply.github.com>
2021-08-21 14:27:15 +0530

    pratik showpassword commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -120,17 +120,17 @@ export default function Login(){
                             </div>
                             <div className="form">
                                 <div className="form-group">
-                                <label htmlFor="username">Username</label>
-                                {/* <input type="text" name="username" placeholder="username" /> */}
-                                <input required type="email" className="form-control input-line" placeholder="username" onChange={(e) => setemail(e.target.value)}/>
+                                    <label htmlFor="username">Username</label>
+                                        <input required type="email" className="form-control input-line" placeholder="username" onChange={(e) => setemail(e.target.value)}/>
                                 </div>
                                 <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input required minLength="5"  type={showPassword===true?"text":"password"} className="form-control input-line" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
-                                <span>
-                                    <i class="fas fa-eye" onClick={(e)=>setShowPassword(!showPassword)}></i>
-                                </span>
-                                </div>
+                                        <label htmlFor="password">Password</label>
+                                        <input required minLength="5"  type={showPassword===true?"text":"password"} className="form-control input-line" aria-describedby="basic-addon2" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+                                    <span>
+                                        <i class="fas fa-eye" onClick={(e)=>setShowPassword(!showPassword)}></i>
+                                    </span>
+                                </div>   
+                                
                             </div>
                             <div className="footer">
                                 <button type="submit" className="btn">

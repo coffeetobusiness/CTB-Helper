5c933319e65994efff03dc4525359b72aab2c30b
pratikparamane98 <56255793+pratikparamane98@users.noreply.github.com>
2021-08-26 10:02:33 +0530

    pp

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,7 @@ import { signin } from '../redux/actions/auth';
 import { Link  } from "react-router-dom";
 import Header from '../header/Header';
 import { CredentialsContext } from "../App";
+import LoginImg from '../../src/login.svg';
 
 
 export const handleErrors = async (response) => {
@@ -22,6 +23,8 @@ export default function Login(){
 
     const [postData, setPostData] = useState({ email: '', password: '' });
     const [, setCredentials] = useContext(CredentialsContext);
+    const [showPassword,setShowPassword] = useState(false)
+    const [error, setError] = useState("");
 
     
    
@@ -38,45 +41,101 @@ export default function Login(){
           
       };
 
-    return(
-        <div className="app">
-        <div><Header/></div> 
-        <div className="row App-conatiner">
-            <div className="col-6 container-fluid">
-               <form onSubmit={handleSubmit}>
+    // return(
+    //     <div className="app">
+    //     <div><Header/></div> 
+    //     <div className="row App-conatiner">
+    //         <div className="col-6 container-fluid">
+    //            <form onSubmit={handleSubmit}>
 
-                <h3><i class="fas fa-hands-helping"></i> Helpo LogIn</h3>
+    //             <h3><i class="fas fa-hands-helping"></i> Helpo LogIn</h3>
                
-                <p className=" mt-5">
-                    New User?? <Link  to="/register">Register</Link>
-                </p>
+    //             <p className=" mt-5">
+    //                 New User?? <Link  to="/register">Register</Link>
+    //             </p>
                 
-                <div className="form-group">
-                    <input required type="email" className="form-control input-line" placeholder="email" value={postData.email} onChange={(e) => setPostData({ ...postData, email: e.target.value })}/>
-                </div>
+    //             <div className="form-group">
+    //                 <input required type="email" className="form-control input-line" placeholder="email" value={postData.email} onChange={(e) => setPostData({ ...postData, email: e.target.value })}/>
+    //             </div>
 
-                <div className="form-group">
-                    <input required minLength="5" type="password" className="form-control input-line" placeholder="password" value={postData.password} onChange={(e) => setPostData({ ...postData, password: e.target.value })}/>
-                </div>
+    //             <div className="form-group">
+    //                 <input required minLength="5" type="password" className="form-control input-line" placeholder="password" value={postData.password} onChange={(e) => setPostData({ ...postData, password: e.target.value })}/>
+    //             </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                    </div>
-                </div>
+    //             <div className="form-group">
+    //                 <div className="custom-control custom-checkbox">
+    //                     <input type="checkbox" className="custom-control-input" id="customCheck1" />
+    //                 </div>
+    //             </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+    //             <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
 
-                {/* {error && <span id="reg-msg" >{error}</span>} */}
+    //             {/* {error && <span id="reg-msg" >{error}</span>} */}
 
-                <p className="forgot-password text-right">
-                    Forgot <Link  to="/reset">password</Link>
-                </p>
-            </form>
+    //             <p className="forgot-password text-right">
+    //                 Forgot <Link  to="/reset">password</Link>
+    //             </p>
+    //         </form>
+    //         </div>
+    //         <div className="col" >
+    //         </div>
+    //     </div>
+    //     </div>
+    // )
+
+
+
+    return(
+        <div className="background">
+            <div className="container">
+            <div className="base-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="content">
+                        <div className="header">Welcome Back</div>
+
+                            <div className="image">
+                                <img src={LoginImg} />
+                            </div>
+                            <div className="form">
+                                <div className="form-group">
+                                    {/* <label htmlFor="username">Username</label> */}
+                                        <input required type="email" className="form-control input-line" placeholder="username" value={postData.email} onChange={(e) => setPostData({ ...postData, email: e.target.value })}/>
+                                </div>
+                                <div className="form-group">
+                                        {/* <label htmlFor="password">Password</label> */}
+                                        <input required minLength="5"  type={showPassword===true?"text":"password"} className="form-control input-line" aria-describedby="basic-addon2" placeholder="password" value={postData.password} onChange={(e) => setPostData({ ...postData, password: e.target.value })}/>
+                                    <span>
+                                        <i class="fas fa-eye" onClick={(e)=>setShowPassword(!showPassword)}></i>
+                                    </span>
+                                </div>   
+                                
+                            </div>
+                            <div className="footer">
+                                <button type="submit" className="btn">
+                                    Login
+                                </button>
+                            </div>
+                            <div className="error">
+                                {error && <span id="reg-msg" >{error}</span>}
+                            </div>
+                            <div className="password">
+                                <p>forgot your password?<Link  to="/reset">Reset password</Link></p>
+                            </div>
+                            <div className="account">
+                                {/* <p>-------------------or-------------------</p> */}
+                                <p>or</p>
+                            </div>
+                            <div className="account">
+                                <p><Link  to="/register">Create New Account</Link></p>
+                            </div>
+                    </div> 
+                </form>
             </div>
-            <div className="col" >
-            </div>
+            {/* <div className="base-container2">
+            </div> */}
+            
         </div>
         </div>
     )
+
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -13,6 +13,8 @@ export default function Register(){
   const [postData, setPostData] = useState({ firstName: '', lastName: '', email: '', password: '', password2: '' });
     
   const [error, setError] = useState(""); 
+  const [showPassword,setShowPassword] = useState(false)
+//   const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
         e.preventDefault();
@@ -28,45 +30,96 @@ export default function Register(){
         } 
     };
 
-    return(
-        <div className="app">
-        <div><Header/></div> 
-        <div className="row App-conatiner">
-            <div className="col-6">
+    // return(
+    //     <div className="app">
+    //     <div><Header/></div> 
+    //     <div className="row App-conatiner">
+    //         <div className="col-6">
            
-               <form onSubmit={handleSubmit}>
+    //            <form onSubmit={handleSubmit}>
 
-                <h3><i class="fas fa-hands-helping"></i> Helpo Register</h3>
+    //             <h3><i class="fas fa-hands-helping"></i> Helpo Register</h3>
                 
                 
 
-                <div className="form-group mt-5">
-                    <input value={postData.firstName} label="firstName" name="firstName" required minLength="3" type="text" className="form-control input-line" placeholder="firstname" onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
-                </div>
-                <div className="form-group">
-                    <input value={postData.lastName} name="lastName" label="lastName" required minLength="3" type="text" className="form-control input-line" placeholder="lastname" onChange={(e) => setPostData({ ...postData, lastName: e.target.value })}/>
-                </div>
+    //             <div className="form-group mt-5">
+    //                 <input value={postData.firstName} label="firstName" name="firstName" required minLength="3" type="text" className="form-control input-line" placeholder="firstname" onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
+    //             </div>
+    //             <div className="form-group">
+    //                 <input value={postData.lastName} name="lastName" label="lastName" required minLength="3" type="text" className="form-control input-line" placeholder="lastname" onChange={(e) => setPostData({ ...postData, lastName: e.target.value })}/>
+    //             </div>
              
-                <div className="form-group">
-                    <input value={postData.email} name="email" label="email" required type="email" className="form-control input-line" placeholder="email" onChange={(e) => setPostData({ ...postData, email: e.target.value })}/>
-                </div>
-                <div className="form-group">
-                    <input value={postData.password} name="password" label="password" required minLength="5" type="text" className="form-control input-line" placeholder="password" onChange={(e) => setPostData({ ...postData, password: e.target.value })}/>
-                </div>
-                <div className="form-group">
-                    <input value={postData.password2} name="password2" label="password2" required minLength="5" type="text" className="form-control input-line" placeholder="confirm password" onChange={(e) => setPostData({ ...postData, password2: e.target.value })}/>
-                </div>
+    //             <div className="form-group">
+    //                 <input value={postData.email} name="email" label="email" required type="email" className="form-control input-line" placeholder="email" onChange={(e) => setPostData({ ...postData, email: e.target.value })}/>
+    //             </div>
+    //             <div className="form-group">
+    //                 <input value={postData.password} name="password" label="password" required minLength="5" type="text" className="form-control input-line" placeholder="password" onChange={(e) => setPostData({ ...postData, password: e.target.value })}/>
+    //             </div>
+    //             <div className="form-group">
+    //                 <input value={postData.password2} name="password2" label="password2" required minLength="5" type="text" className="form-control input-line" placeholder="confirm password" onChange={(e) => setPostData({ ...postData, password2: e.target.value })}/>
+    //             </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
-                {error && <span id="reg-msg" >{error}</span>}
-                <p className=" text-right">
-                    Back to <Link  to="/">Login</Link>
-                </p>
-            </form>
-            </div>
-            <div className="col" id="col-side">
+    //             <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+    //             {error && <span id="reg-msg" >{error}</span>}
+    //             <p className=" text-right">
+    //                 Back to <Link  to="/">Login</Link>
+    //             </p>
+    //         </form>
+    //         </div>
+    //         <div className="col" id="col-side">
                 
+    //         </div>
+    //     </div>
+    //     </div>
+    // )
+
+
+    return(
+        <div className="background">
+            <div className="container">
+            <div className="base-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="content">
+                        <div className="header">Register</div>
+
+                            {/* <div className="image">
+                                <img src={LoginImg} />
+                            </div> */}
+                            <div className="form">
+                                <div className="form-group">
+                                        <input required type="text" className="form-control input-line" placeholder="First Name" value={postData.firstName} onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
+                                </div>
+                                <div className="form-group">
+                                        <input required type="text" className="form-control input-line" placeholder="Last Name" value={postData.firstName} onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
+                                </div><div className="form-group">
+                                        <input required type="email" className="form-control input-line" placeholder="Email" value={postData.firstName} onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
+                                </div><div className="form-group">
+                                        <input required type="password" className="form-control input-line" placeholder="Password" value={postData.firstName} onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
+                                        
+                                </div>
+                                <div className="form-group">
+                                        <input required minLength="5" type="password"  className="form-control input-line" aria-describedby="basic-addon2" placeholder="Confirm" value={postData.firstName} onChange={(e) => setPostData({ ...postData, firstName: e.target.value })}/>
+                                </div>   
+                                
+                            </div>
+                            <div className="footer">
+                                <button type="submit" className="btn">
+                                    Sign Up
+                                </button>
+                            </div>
+                            <div className="error">
+                                {error && <span id="reg-msg" >{error}</span>}
+                            </div>
+                            <div className="password">
+                                <p>Already have an account?<Link  to="/">Sign In</Link></p>
+                            </div>
+                            
+                    </div> 
+                </form>
             </div>
+            {/* <div className="base-container2">
+            </div> */}
+            
         </div>
         </div>
     )
